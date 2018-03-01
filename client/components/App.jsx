ffaec765024ae3a690e6208b3c297877b91511df
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-01 02:07:23 +0000

    added: fetchQuestion comonent added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,8 @@ import React from "react";
 
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Questions from "./Question";
+
 const Dashboard = () => <h1>404 Status , Page NOT FOUND !!!</h1>;
 
 const App = () => (
