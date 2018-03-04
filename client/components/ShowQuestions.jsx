c04be3205bbb4805c1708d6b2b01a331dd34e2f7
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-04 13:25:02 +0000

    added: Material UI config added along with Reactjs Reference

--- client/components/ShowQuestions.jsx
+++ client/components/ShowQuestions.jsx
@@ -8,15 +8,48 @@ import * as actions from "../actions";
 
 
 class ShowQuestions extends Component {
+
+	// Fetching Questions Data 
+	
 	componentDidMount() {
 		this.props.fetchQuestion();
 	}
 
-	render() {
+	// onSubmit Form values selected by the user will go to the Show Results Component 
+	onSubmit = values => {
+		this.answers.method(values);
 
+	};
+	render() {
+		let formData;
+
+		if (this.props.mcqs) {
+			formData = this.props.mcqs;
+		} else {
+			formData = null;
+		}
 		console.log(this.props.mcqs);
 
-		return (<div>Form Data is Working</div>);
+	/* Reference added (<Answers onRef={ref => (this.answers = ref)} /> )
+	to send the user input values to the formData 
+	More on Reactjs Ref https://reactjs.org/docs/refs-and-the-dom.html */
+
+		return formData !== null ? (
+			<MuiThemeProvider muiTheme={getMuiTheme()}>
+				<div style={{ padding: 15 }}>
+					<h2>Elders Coding Challenge</h2>
+					<Answers onRef={ref => (this.answers = ref)} />
+					<Form
+						formData={formData}
+						onSubmit={e => {
+							this.onSubmit(e);
+						}}
+					/>
+				</div>
+			</MuiThemeProvider>
+		) : (
+			<div>Loading</div>
+		);
 	}
 }
 
