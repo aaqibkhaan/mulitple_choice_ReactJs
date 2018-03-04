fd41d4b670de280e2286c5783b35fd0aef8e43b7
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-04 12:55:15 +0000

    added: ShowResults Component

---
+++ client/components/ShowResults.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable  */
+
+/* eslint-disable  */
+
+import React, { Component } from "react";
+
+import axios from "axios";
+
+import ShowModal from "../components/showModal";
+
+class Answers extends Component {
+
+
+	
+	render() {
+		return (
+			<div>
+				<ShowModal/>
+			</div>
+		);
+	}
+}
+
+export default Answers;
