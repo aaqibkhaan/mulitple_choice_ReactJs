50b2b4384a00bc0a6891c671b6d26ec1235e9151
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-04 13:02:59 +0000

    added: axios request added to get the correctAnswers

--- client/components/ShowResults.jsx
+++ client/components/ShowResults.jsx
@@ -12,15 +12,24 @@ class Answers extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			correctAnswers: [],
 			userAnswers: []
-		};
+				};
 	}
-	
+
 	/* Reference added to get the user values from the formData 
 	More on Reactjs Ref https://reactjs.org/docs/refs-and-the-dom.html */
 
 	componentDidMount() {
 		this.props.onRef(this);
+		axios
+			.get(
+				"https://gist.githubusercontent.com/aaqibkhaan/31633269daa07186578c72f18578def3/raw/35db68c551aa0252bbbbecc5d74b219c2bdbb899/answers.js"
+
+			)
+			.then(resp => {
+				this.setState({ correctAnswers: resp.data.answers });
+			});
 	}
 	componentWillUnmount() {
 		this.props.onRef(undefined);
@@ -30,13 +39,15 @@ class Answers extends Component {
 	}
 	
 	render() {
-
 		const userAnswers = this.state.userAnswers;
-		console.log(userAnswers);
+		const correctAnswers = this.state.correctAnswers;
+		console.log(correctAnswers);
 		return (
 			<div>
 				<ShowModal
-					userAnswers={userAnswers}
+					total={Total}
+					wrongAnswers={findingCorrectAnswer}
+					correctAnswers = {correctAnswers}
 				/>
 			</div>
 		);
