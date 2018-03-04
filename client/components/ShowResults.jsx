19501045efd5510c27d6d2ed08b9e976722d0b28
aaqibkhaan <aaqibrashid@hotmail.com>
2018-03-04 12:59:28 +0000

    added: Ref added to get the user Values

--- client/components/ShowResults.jsx
+++ client/components/ShowResults.jsx
@@ -9,13 +9,35 @@ import axios from "axios";
 import ShowModal from "../components/showModal";
 
 class Answers extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			userAnswers: []
+		};
+	}
+	
+	/* Reference added to get the user values from the formData 
+	More on Reactjs Ref https://reactjs.org/docs/refs-and-the-dom.html */
 
-
+	componentDidMount() {
+		this.props.onRef(this);
+	}
+	componentWillUnmount() {
+		this.props.onRef(undefined);
+	}
+	method(values) {
+		this.setState({ userAnswers: values });
+	}
 	
 	render() {
+
+		const userAnswers = this.state.userAnswers;
+		console.log(userAnswers);
 		return (
 			<div>
-				<ShowModal/>
+				<ShowModal
+					userAnswers={userAnswers}
+				/>
 			</div>
 		);
 	}
